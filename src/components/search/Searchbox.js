c983b0198f68f4d21ad09d6e7250bddc47087bef
Jessica Giehl <sumdian@gmail.com>
2019-02-14 13:58:54 -0500

    added working search box

--- src/components/search/Searchbox.js
+++ src/components/search/Searchbox.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 
 class Searchbox extends Component {
 
+  state = {
+    "query" : ""
+  };
+
+  handleChange = e => {
+    this.setState({"query": e.target.value})
+  };
+
 
   render() {
     return (
       <form>
-        <input type="text" onChange={this.props.searchChange} />
-        <button onClick={this.props.searchSubmit}>Search</button>
+        <input type="text" value={this.state.query} onChange={this.handleChange} />
+        <button onClick={(e)=>{e.preventDefault();this.props.history.push("/search/"+this.state.query)}}>Search</button>
       </form>
     )
   }
