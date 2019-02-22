b19555e4b60b9a6f5705f122274fc749a0fe2d6c
Sean Bernath <sbernath@gmail.com>
2019-02-21 20:24:36 -0500

    Updates

---
+++ precache-manifest.07408ecc59890469d57ff99ab919d360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7016db365237396a1d69",
+    "url": "/MealPlanner/static/js/runtime~main.7016db36.js"
+  },
+  {
+    "revision": "de35118916abdf6aca78",
+    "url": "/MealPlanner/static/js/main.de351189.chunk.js"
+  },
+  {
+    "revision": "6e076965141e508d1333",
+    "url": "/MealPlanner/static/js/1.6e076965.chunk.js"
+  },
+  {
+    "revision": "de35118916abdf6aca78",
+    "url": "/MealPlanner/static/css/main.9bfe48ab.chunk.css"
+  },
+  {
+    "revision": "5e6fdb724c10538b03ee5e4a48dacd73",
+    "url": "/MealPlanner/index.html"
+  }
+];
