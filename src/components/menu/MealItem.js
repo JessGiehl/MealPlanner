31f9e043e27774c21b5d6b58e4ce53207c08ceaa
Jessica Giehl <sumdian@gmail.com>
2019-02-28 19:17:39 -0500

    fixed image width on menu view

--- src/components/menu/MealItem.js
+++ src/components/menu/MealItem.js
@@ -6,7 +6,7 @@ const MealItem = (props) => {
     return(
       //use the props to pass down the properties or each element and a reference to the app.js method for deletion in the button
         <li class="border border-dark rounded col-md-4 m-3 p-2">
-          <img src={props.item.image} alt={props.item.name}></img>
+          <img style={styles.img} src={props.item.image} alt={props.item.name}></img>
           <h5>{props.item.name}</h5>
           <Link to={"../recipe/" + props.item.id}>Recipe Details</Link>
           <br></br>
@@ -16,3 +16,9 @@ const MealItem = (props) => {
 }
 
 export default MealItem
+
+const styles = {
+  img: {
+    maxWidth: "100%"
+  }
+}
